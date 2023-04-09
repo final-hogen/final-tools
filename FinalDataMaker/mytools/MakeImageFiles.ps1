2158cce3afc25da4f7cb0ab40894d9e3021d3123
final-hogen <hoggenn@gmail.com>
2023-04-09 10:51:08 +0900

    事前にチェックするように修正

--- FinalDataMaker/mytools/MakeImageFiles.ps1
+++ FinalDataMaker/mytools/MakeImageFiles.ps1
@@ -2,14 +2,16 @@
 #画像をコピーする
 $webpFolder = "画像/"
 $pngFolder = "画像png/"
+$mapperdir = "出力json"
+$mappers = @("filemapperパイロット.json","filemapperスキル.json","filemapperパーツ.json","filemapper戦艦艦載砲.json","filemapper戦艦バフ.json")
 $waifu2x="waifu2x-caffe\waifu2x-caffe-cui.exe"
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 
-mytools/CopyMapperFiles.ps1 "出力json/filemapperパイロット.json"
-mytools/CopyMapperFiles.ps1 "出力json/filemapperスキル.json"
-mytools/CopyMapperFiles.ps1 "出力json/filemapperパーツ.json"
-mytools/CopyMapperFiles.ps1 "出力json/filemapper戦艦艦載砲.json"
-mytools/CopyMapperFiles.ps1 "出力json/filemapper戦艦バフ.json"
-
+foreach( $name in $mappers){
+  $path = "$mapperdir/$name"
+  if(Test-Path $path){
+    mytools/CopyMapperFiles.ps1 $path
+  }
+}
 Write-Host "convert png to webp start."
 Start-Process -FilePath $waifu2x -ArgumentList "-i $pngFolder -o $webpFolder -s 1 -e webp -q 80" -NoNewWindow -Wait
